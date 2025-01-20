5b9770c1f2f5b1d489f3cae51a2a0478a4b996d3
edupoch <edupoch@gmail.com>
2025-01-20 21:05:28 +0100

    Traducciones

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,15 +3,18 @@ module.exports = {
     leyenda: "Paisaje del auga",
     legales: "Textos legales",
     vermas: "Ver más",
+    inicio: "Inicio",
   },
   en: {
     leyenda: "Waterscape",
     legales: "Legal notice",
     vermas: "See more",
+    inicio: "Home",
   },
   gl: {
     leyenda: "Paisaxe da auga",
     legales: "Textos legales",
     vermas: "Ver máis",
+    inicio: "Inicio",
   },
 };
