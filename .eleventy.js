0f089ccf80dce138689ab86d7ee367cf596e4d24
Javi <javicocinitas@gmail.com>
2025-01-21 11:49:46 +0100

    passthrow pdf, fuente y cambio de color en logo

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,8 @@ module.exports = async function (eleventyConfig) {
   // PassThroughCopy
   eleventyConfig.addPassthroughCopy("src/assets/css/tailwindcss.css");
   eleventyConfig.addPassthroughCopy("src/assets/img");
+  eleventyConfig.addPassthroughCopy("src/assets/font");
+  eleventyConfig.addPassthroughCopy("src/pdf");
   eleventyConfig.addPassthroughCopy({
     "node_modules/alpinejs/dist/cdn.min.js": "/assets/alpine.js",
   });
@@ -26,25 +28,19 @@ module.exports = async function (eleventyConfig) {
   });
   // Page Sorting
   eleventyConfig.addCollection("atributo_es", function (collection) {
-    return collection
-      .getFilteredByGlob("./src/es/atributos/*.md")
-      .sort(function (a, b) {
-        return a.data.order - b.data.order;
-      });
+    return collection.getFilteredByGlob("./src/es/atributos/*.md").sort(function (a, b) {
+      return a.data.order - b.data.order;
+    });
   });
   eleventyConfig.addCollection("atributo_en", function (collection) {
-    return collection
-      .getFilteredByGlob("./src/en/atributos/*.md")
-      .sort(function (a, b) {
-        return a.data.order - b.data.order;
-      });
+    return collection.getFilteredByGlob("./src/en/atributos/*.md").sort(function (a, b) {
+      return a.data.order - b.data.order;
+    });
   });
   eleventyConfig.addCollection("atributo_gl", function (collection) {
-    return collection
-      .getFilteredByGlob("./src/gl/atributos/*.md")
-      .sort(function (a, b) {
-        return a.data.order - b.data.order;
-      });
+    return collection.getFilteredByGlob("./src/gl/atributos/*.md").sort(function (a, b) {
+      return a.data.order - b.data.order;
+    });
   });
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
   return {
